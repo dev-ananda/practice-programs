6cc1c6489e6fcf45fb440baa58dea18d86866ed5
ananda <anandagowdan3@gmail.com>
2020-02-15 17:22:15 +0530

    Updated file name. Also log to better understand the output.

--- sampleprograms/src/main/java/dev/ananda/graph/socialnetwork/FriendSuggestorApp.java
+++ sampleprograms/src/main/java/dev/ananda/graph/socialnetwork/FriendSuggestorApp.java
@@ -2,10 +2,11 @@ package dev.ananda.graph.socialnetwork;
 
 import java.util.Set;
 
-public class FriedSuggestorApp {
+public class FriendSuggestorApp {
 	public static void main(String[] args) {
 		SocialNetwork server = new SocialNetwork();
 		Set<User> suggestions = server.getFriendSuggestions(6);
+		System.out.println("Suggested friends are....");
 		suggestions.forEach(System.out::println);
 	}
 }
